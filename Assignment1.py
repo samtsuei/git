0cf859fbca007d84eef47a09e68478bc18978229
samtsuei <60580822+samtsuei@users.noreply.github.com>
2020-02-08 02:52:55 -0500

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,8 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(0,2*np.pi,0.1)   # start,stop,step
+
+x = np.arange(0, 2 * np.pi, 0.1)
 y = np.sin(x)
 z = np.cos(x)
-plt.plot(x,y,x,z)
+plt.plot(x, y, x, z)
 plt.show()
